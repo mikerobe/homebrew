ca76a2ae78f60ee503a6743085652d3262c6a3e3
Mike Robertson <mikerobe@me.com>
2012-03-17 13:09:20 -0400

    modify vim formula to use mercurial HEAD only

--- Library/Formula/vim.rb
+++ Library/Formula/vim.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Vim < Formula
-  url 'ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2'
+  head 'https://vim.googlecode.com/hg/vim', :using => :hg
   homepage 'http://www.vim.org'
-  md5 '5b9510a17074e2b37d8bb38ae09edbf2'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--enable-cscope",
+                          "--enable-rubyinterp",
+                          "--enable-python3interp=yes",
+                          "--enable-perlinterp=yes"
     system "make install"
   end
 
